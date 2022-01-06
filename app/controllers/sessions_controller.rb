5458c160104a7f59f0c28c0b7f9aa29cf07c5db8
hantt-2856 <nguyen.thi.thanh.ha@sun-asterisk.com>
2022-01-06 17:40:46 +0700

    Signin/Signout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    user = User.find_by email: params[:session][:email]
+    if user&.authenticate params[:session][:password]
+      flash[:success] = t ".login_success"
+      log_in user
+      redirect_to root_path
+    else
+      flash.now[:warning] = t ".login_fail"
+      render :new
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_path
+  end
+end
