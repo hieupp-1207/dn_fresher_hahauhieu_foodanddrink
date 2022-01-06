3a10ee86f677b594470c91db9ea30d808eea31a8
hantt-2856 <nguyen.thi.thanh.ha@sun-asterisk.com>
2022-01-06 17:40:46 +0700

    Add food to cart

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
   private
-    def find_user
-      @user = User.find_by id: params[:id]
-      return if @user
 
-      flash[:warning] = t "error.not_found"
-      redirect_to root_path
-    end
+  def find_user
+    @user = User.find_by id: params[:id]
+    return if @user
+
+    flash[:warning] = t "error.not_found"
+    redirect_to root_path
+  end
 end
